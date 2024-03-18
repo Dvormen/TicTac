41de5728a3ff139ecb7d65bc33640d865c1d0336
tomanova <tomanova@spsejecna.cz>
2024-03-18 10:53:31 +0100

    added some shitty things and methods

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Manager {
+    private String direction;
     Scanner sc = new Scanner(System.in);
     TextReader textReader = new TextReader();
     Player player;
@@ -8,9 +9,45 @@ public class Manager {
     public Manager(){
         textReader.reader();
         Player p = new Player(sc.nextLine());
+        map();
 
     }
 
+    public void map(){
+        System.out.println(".  .  .");
+        System.out.println(".  .  .");
+        System.out.println(".  .  .");
+    }
 
+    public String getDirection() {
+        return direction;
+    }
 
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Scanner getSc() {
+        return sc;
+    }
+
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public TextReader getTextReader() {
+        return textReader;
+    }
+
+    public void setTextReader(TextReader textReader) {
+        this.textReader = textReader;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
