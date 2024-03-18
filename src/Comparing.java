2e498123a0237e156476efff331d4b8cb82b1113
dvorak13 <luky.dvorak07@gmail.com>
2024-03-18 10:31:42 +0100

    added class Comparing added method winCondicion

---
+++ src/Comparing.java
@@ -0,0 +1,20 @@
+public class Comparing {
+
+    private int lenght = 3;
+    private int width = 3;
+    String numbers [][] = new String[width][lenght];
+
+
+
+    public void winCondicion(){
+        if((numbers[0][0] == "x") && (numbers[0][1] == "x") && (numbers[0][2] == "x")){
+
+        } else if ((numbers[1][0] == "x") && (numbers[1][1] == "x") && (numbers[1][2] == "x")) {
+
+        } else if ((numbers[2][0] == "x") && (numbers[2][1] == "x") && (numbers[2][2] == "x")) {
+
+        } else if ((numbers[0][0] == "x") && (numbers[1][0] == "x") && (numbers[0][2] == "x")) {
+
+        }
+    }
+}
