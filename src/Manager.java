85c1a6a63ca644579c3385df571ba61b73e65d9c
tomanova <tomanova@spsejecna.cz>
2024-03-18 10:35:35 +0100

    added constructor

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Manager {
+    Scanner sc = new Scanner(System.in);
+    TextReader textReader = new TextReader();
+    Player player;
+
+    public Manager(){
+        textReader.reader();
+        Player p = new Player(sc.nextLine());
+
+    }
+
+
 
 }
